3a872961ec34d5db0b49e6e44290028f00633508
Brian Hardy <hardyrz@kzsoftworks.com>
2017-11-04 20:57:28 -0300

    Notifications popover

--- Conecta/src/app/footer/footer.component.ts
+++ Conecta/src/app/footer/footer.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  state = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  stateHandler(newState) {
+    this.state = newState;
   }
 
 }
