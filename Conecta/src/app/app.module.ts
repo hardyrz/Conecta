3abc3b12ceeec07367808b37e4e7ad520ebf4b22
Ariel Erlijman <erlijman@gmail.com>
2017-11-04 16:53:54 -0300

    main components

--- Conecta/src/app/app.module.ts
+++ Conecta/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { BodyMainComponent } from './body-main/body-main.component';
+import { BodyTitleComponent } from './body-title/body-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    BodyMainComponent,
+    BodyTitleComponent
   ],
   imports: [
     BrowserModule
