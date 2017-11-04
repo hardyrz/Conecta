9cde91dfa5f585f5634a19d53b8e5cf7f02da880
Ariel Erlijman <erlijman@gmail.com>
2017-11-04 18:32:19 -0300

    novedades para juan

--- Conecta/src/app/novedad/novedad.component.ts
+++ Conecta/src/app/novedad/novedad.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-novedad',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./novedad.component.css']
 })
 export class NovedadComponent implements OnInit {
+
+  @Input() image = '';
+  @Input() title = '';
+  @Input() text = '';
 
   constructor() { }
 
