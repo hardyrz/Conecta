0671c0e02e36da1941228d1087b83c6e993c5c49
Brian Hardy <hardyrz@kzsoftworks.com>
2017-11-04 21:02:06 -0300

    Notifications by user

--- Conecta/src/app/footer/footer.component.ts
+++ Conecta/src/app/footer/footer.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   state = false;
+  usuario = '';
   constructor() { }
 
   ngOnInit() {
+    this.usuario = localStorage.getItem("usuario");
   }
 
   stateHandler(newState) {
